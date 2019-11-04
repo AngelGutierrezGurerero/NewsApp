6a5304c3d5b5fbb2a3688c1d60875880fb6ee3ca
Andres Niño <nels96@hotmail.es>
2019-11-03 22:53:35 -0500

    service all news full and styles

--- newsApp/src/app/models/list-news.ts
+++ newsApp/src/app/models/list-news.ts
@@ -2,10 +2,13 @@ export class  RootObject {
   status: string;
   totalResults: number;
   articles: Article[];
+  constructor(status: string,totalResults: number,articles: Article[]){
+    this.status=status;
+    this.totalResults=totalResults;
+  }
 }
 
 export class  Article {
-  source: Source;
   author: string;
   title: string;
   description: string;
@@ -13,6 +16,15 @@ export class  Article {
   urlToImage: string;
   publishedAt: string;
   content: string;
+  constructor(author: string,title: string,description: string,url: string, urlToImage: string,publishedAt: string,content: string){
+    this.author=author;
+    this.title=title;
+    this.description=description;
+    this.url=url;
+    this.urlToImage=urlToImage;
+    this.publishedAt=publishedAt;
+    this.content=content;
+  }
 }
 
 export class  Source {
--- newsApp/src/app/tab1/tab1.page.ts
+++ newsApp/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NewsService } from '../services/news.service';
+import { RootObject, Article } from '../models/list-news';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,9 @@ import { NewsService } from '../services/news.service';
 })
 export class Tab1Page {
   
-  Article;
+  listNew: Array<Article>= new Array();
+  
+  allNews: any[] =[];
   constructor(private newsService:NewsService) {
 
 
@@ -16,9 +19,31 @@ export class Tab1Page {
   ionViewDidEnter(){
 
     this.newsService.getNews().subscribe((data)=>{
-      console.log(data);
-      this.Article = data['articles'];
+      for (let clave in data['articles']){
+        // console.log(data['articles'][clave]);
+        // console.log(data['articles'][clave]['author']);
+
+        this.listNew.push(new Article(
+        data['articles'][clave]['author'],
+        data['articles'][clave]['title'],
+        data['articles'][clave]['description'],
+        data['articles'][clave]['url'],
+        data['articles'][clave]['urlToImage'],
+        data['articles'][clave]['publishedAt'],
+        data['articles'][clave]['content']));
+        console.log((this.listNew.length))
+        
+      }
+      
+      for(var i = 0; i < this.listNew.length; i++){ 
+      console.log(this.listNew[i]); // output: Apple Orange Banana
+    }
+    return this.listNew ;
     });
+ 
+    
+    
   }
+  
 
 }
